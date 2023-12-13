ef4124e531670c6652303517bc9d57da0b76c9d2
Aksachlisimo <simo_akk@hotmail.com>
2023-12-13 20:58:27 +0100

    yes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://smart-api-v1.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
